fcebce4ac2e49353345d5f721944facb1c017cf3
TerryLTY <terrylty18@gmail.com>
2025-09-27 18:43:59 -0400

    implimenté GSAP

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".logoprincipal img", {
+    opacity: 1,
+    ease: "power1.inOut",
+    duration: 2,
+})
+
+gsap.to(".monnom", {
+    translateX: "1%",
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".logoprincipal",
+        start: "top 30%",
+        end: "bottom top",
+        scrub: 1
+    }
+});
+
+gsap.fromTo(
+    ".liste",
+    { alpha: 0, y: 30 },
+    {
+        alpha: 1,
+        y: -30,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: ".liste",
+            start: "top 40%",
+            end: "top 30%",
+            scrub: 1,
+            markers: true
+        }
+    }
+);
